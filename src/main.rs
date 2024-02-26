c8308a8903a5e1d2ab7c15d04acb8803ffb8c0a7
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2024-02-27 00:27:53 +0530

    handle pong

--- src/main.rs
+++ src/main.rs
@@ -32,11 +32,12 @@ fn handle_connection(mut stream: TcpStream) {
         match stream.read(&mut read_buf) {
             Ok(n) => {
                 // Check if no bytes were read from the stream
-                if n == 0 {
-                    println!("Sleeping to read more data");
-                    thread::sleep(std::time::Duration::from_millis(500));
-                    continue;
-                }
+                // if n == 0 {
+                //     println!("Sleeping to read more data");
+                //     thread::sleep(std::time::Duration::from_millis(500));
+                //     continue;
+                // }
+                handle_data(&mut stream, &read_buf);
                 handle_data(&mut stream, &read_buf);
                 break;
             }
@@ -52,14 +53,14 @@ fn handle_data(stream: &mut TcpStream, read_buf: &[u8]) {
     let ping_response = "+PONG\r\n";
     let ping_response_size = ping_response.len();
 
-    if res.contains("PING") {
-        match stream.write(ping_response.as_bytes()) {
-            Ok(n) if n == ping_response_size => {
-                println!("Successfully ponged!");
-            }
-            _ => {
-                println!("Failed to pong :(")
-            }
+    // if res.contains("PING") {
+    match stream.write(ping_response.as_bytes()) {
+        Ok(n) if n == ping_response_size => {
+            println!("Successfully ponged!");
+        }
+        _ => {
+            println!("Failed to pong :(")
         }
     }
+    // }
 }
