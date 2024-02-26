d79c174edf2122af3815762a0ea58b689a9ffd88
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2024-02-27 01:00:49 +0530

    handle pong

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,6 @@ fn handle_connection(mut stream: TcpStream) {
                 //     continue;
                 // }
                 handle_data(&mut stream, &read_buf);
-                break;
             }
             Err(_) => todo!(),
         }
