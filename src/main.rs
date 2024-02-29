a99c507e5500d29e7d21b724af0d243ff681f721
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2024-02-29 14:04:17 +0530

    hey

--- src/main.rs
+++ src/main.rs
@@ -46,13 +46,12 @@ fn handle_connection(mut stream: TcpStream) {
                 handle_data(&mut stream, &read_buf);
                 thread::sleep(Duration::from_secs_f64(0.5));
                 handle_data(&mut stream, &read_buf);
+                thread::sleep(Duration::from_secs_f64(1.0));
                 break;
             }
             Err(_) => todo!(),
         }
     }
-
-    thread::sleep(Duration::from_secs_f64(0.5));
 }
 
 fn handle_data(stream: &mut TcpStream, read_buf: &[u8]) {
